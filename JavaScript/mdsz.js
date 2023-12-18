782ea0843bf11ac8e7d3c7c62febe4ce692afbeb
Marszy <106674124+lovemarszy@users.noreply.github.com>
2023-12-19 00:08:46 +0800

    Update mdsz.js

--- JavaScript/mdsz.js
+++ JavaScript/mdsz.js
@@ -17,17 +17,17 @@ hostname = api.revenuecat.com
 var py996 = JSON.parse($response.body);
 py996.subscriber.entitlements = {
   "Entitlement.Pro": {
-    "expires_date": "9999-09-09T09:09:09Z",
+    "expires_date": "2099-09-09T09:09:09Z",
     "product_identifier": "tech.miidii.MDClock.subscription.year.v1",
-    "purchase_date": "9999-09-09T09:09:09Z"
+    "purchase_date": "2099-09-09T09:09:09Z"
   }
 };
 py996.subscriber.original_purchase_date = "2023-02-23T03:33:33Z";
 py996.subscriber.subscriptions = {
   "tech.miidii.MDClock.subscription.year.v1": {
-    "expires_date": "9999-09-09T09:09:09Z",
+    "expires_date": "2099-09-09T09:09:09Z",
     "original_purchase_date": "2023-02-23T02:33:33Z",
-    "purchase_date": "9999-09-09T09:09:09Z",
+    "purchase_date": "2099-09-09T09:09:09Z",
     "ownership_type": "PURCHASED",
     "store": "app_store"
   }
